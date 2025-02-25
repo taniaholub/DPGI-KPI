cd554912133ac3a15d1fe562a6abbbda22c2c145
Tania Holub <taniagolub2004@gmail.com>
2025-02-25 17:26:12 +0200

    add Task4

--- Lab2/MainWindow.xaml.cs
+++ Lab2/MainWindow.xaml.cs
@@ -1,28 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace Lab2
+namespace Lab2 { 
+public partial class TextEditor : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public TextEditor()
     {
-        public MainWindow()
+        InitializeComponent();
+
+        var openBinding = new CommandBinding(ApplicationCommands.Open,
+        Execute_Open, CanExecute_Open);
+        var closeBinding = new CommandBinding(ApplicationCommands.Close,
+        Execute_Close, CanExecute_Close);
+        var saveBinding = new CommandBinding(ApplicationCommands.Save,
+        Execute_Save, CanExecute_Save);
+        CommandBindings.Add(openBinding);
+        CommandBindings.Add(closeBinding);
+        CommandBindings.Add(saveBinding);
+    }
+    private void CanExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+    {
+        e.CanExecute = MainTextBox.Text.Trim().Length > 0;
+    }
+    private void Execute_Save(object sender, ExecutedRoutedEventArgs e)
+    {
+        File.WriteAllText("Lab2File.txt", MainTextBox.Text);
+        MessageBox.Show("The file was saved!");
+    }
+
+    private void CanExecute_Open(object sender, CanExecuteRoutedEventArgs e)
+    {
+        e.CanExecute = MainTextBox.Text.Trim().Length == 0;
+    }
+    private void Execute_Open(object sender, ExecutedRoutedEventArgs e)
+    {
+        const string filter = "Text files (*.txt)|*.txt;|All files (*.*)|*.*";
+
+        var openDialog = new OpenFileDialog()
         {
-            InitializeComponent();
+            InitialDirectory = AppDomain.CurrentDomain.BaseDirectory,
+            Filter = filter
+        };
+        if (openDialog.ShowDialog() == true)
+        {
+            MainTextBox.Text = File.ReadAllText(openDialog.FileName);
         }
     }
+
+    private void CanExecute_Close(object sender, CanExecuteRoutedEventArgs e)
+    {
+        e.CanExecute = MainTextBox.Text.Length > 0;
+    }
+    private void Execute_Close(object sender, ExecutedRoutedEventArgs e)
+    {
+        MainTextBox.Text = "";
+    }
+}
 }
