077dc2d06de46ffd62fde185b7ebb52f0e118f72
Tania Holub <taniagolub2004@gmail.com>
2025-02-25 10:49:01 +0200

    modify lab1 directory

---
+++ Lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Lab1
+{
+    /// <summary>
+    /// Головний клас програми Lab1.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Точка входу в програму.
+        /// </summary>
+        /// <param name="args">Аргументи командного рядка.</param>
+        static void Main(string[] args)
+        {
+            Hello.SayHello();
+        }
+    }
+
+    /// <summary>
+    /// Клас для виведення привітання.
+    /// </summary>
+    internal class Hello
+    {
+        /// <summary>
+        /// Виводить привітання у консоль.
+        /// </summary>
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
