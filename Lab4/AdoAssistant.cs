015e79f97a875145192cb30efc0af2362289f98f
Tania Holub <taniagolub2004@gmail.com>
2025-04-13 13:12:37 +0300

    lab4_version_1

---
+++ Lab4/AdoAssistant.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Lab4
+{
+    public class AdoAssistant
+    {
+        private readonly string connectionString = "Data Source=TANIAHOLUB\\MSSQLSERVER07;Initial Catalog=DPGI;Integrated Security=True";
+
+        // Завантаження таблиці
+        public DataTable TableLoad()
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                command.CommandText = "SELECT * FROM Clients";
+                adapter.Fill(dataTable);
+            }
+            return dataTable;
+        }
+
+        // Додавання нового запису
+        public void AddClient(string clientId, string clientName, string phoneNumber, string address, decimal orderAmount)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO Clients (ClientId, ClientName, PhoneNumber, Address, OrderAmount) VALUES (@ClientId, @ClientName, @PhoneNumber, @Address, @OrderAmount)", connection);
+                command.Parameters.AddWithValue("@ClientId", clientId); // Виправлено: використовуємо clientId
+                command.Parameters.AddWithValue("@ClientName", clientName);
+                command.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                command.Parameters.AddWithValue("@Address", address);
+                command.Parameters.AddWithValue("@OrderAmount", orderAmount);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // Оновлення запису
+        public void UpdateClient(int clientId, string clientName, string phoneNumber, string address, decimal orderAmount)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("UPDATE Clients SET ClientName = @ClientName, PhoneNumber = @PhoneNumber, Address = @Address, OrderAmount = @OrderAmount WHERE ClientId = @ClientId", connection);
+                command.Parameters.AddWithValue("@ClientId", clientId);
+                command.Parameters.AddWithValue("@ClientName", clientName);
+                command.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                command.Parameters.AddWithValue("@Address", address);
+                command.Parameters.AddWithValue("@OrderAmount", orderAmount);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // Видалення запису
+        public void DeleteClient(int clientId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("DELETE FROM Clients WHERE ClientId = @ClientId", connection);
+                command.Parameters.AddWithValue("@ClientId", clientId);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Lab4/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Data;
+using System.Windows;
+
+namespace Lab4
+{
+    public partial class MainWindow : Window
+    {
+        private AdoAssistant _adoAssistant;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _adoAssistant = new AdoAssistant();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RefreshData();
+        }
+
+        public void RefreshData()
+        {
+            list.ItemsSource = _adoAssistant.TableLoad().DefaultView;
+        }
+
+        private void list_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (list.SelectedItem is DataRowView selected)
+            {
+                ClientIdBox.Text = selected["ClientId"].ToString();
+                ClientNameBox.Text = selected["ClientName"].ToString();
+                PhoneNumberBox.Text = selected["PhoneNumber"].ToString();
+                AddressBox.Text = selected["Address"].ToString();
+                OrderAmountBox.Text = selected["OrderAmount"].ToString();
+            }
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string clientId = ClientIdBox.Text;
+                string clientName = ClientNameBox.Text;
+                string phone = PhoneNumberBox.Text;
+                string address = AddressBox.Text;
+                decimal amount = decimal.Parse(OrderAmountBox.Text);
+
+                _adoAssistant.AddClient(clientId, clientName, phone, address, amount);
+                RefreshData();
+                MessageBox.Show("Клієнта додано.");
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Помилка: " + ex.Message);
+            }
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (list.SelectedItem is DataRowView selected)
+            {
+                try
+                {
+                    int clientId = int.Parse(ClientIdBox.Text);
+                    string clientName = ClientNameBox.Text;
+                    string phone = PhoneNumberBox.Text;
+                    string address = AddressBox.Text;
+                    decimal amount = decimal.Parse(OrderAmountBox.Text);
+
+                    _adoAssistant.UpdateClient(clientId, clientName, phone, address, amount);
+                    RefreshData();
+                    MessageBox.Show("Клієнта оновлено.");
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show("Помилка при оновленні: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Оберіть клієнта для оновлення.");
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (list.SelectedItem is DataRowView selected)
+            {
+                if (MessageBox.Show("Видалити цього клієнта?", "Підтвердження", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    _adoAssistant.DeleteClient((int)selected["ClientId"]);
+                    RefreshData();
+                }
+            }
+        }
+    }
+}
